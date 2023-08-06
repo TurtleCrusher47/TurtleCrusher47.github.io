c07c8e658dd913e48c1224b00cc0b88f5ba10c38
TurtleCrusher47 <111266586+TurtleCrusher47@users.noreply.github.com>
2023-08-06 23:13:08 +0800

    removed hover for mobile screenwidth

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,24 @@
-// var bruh=document.getElementById("heh");
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal")
+    for (var i = 0; i < reveals.length; i++) {
+        // height of the viewport
+        var windowHeight = window.innerHeight;
+        // distance of the element from the top of the viewport
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        // height which the element should reveal itself
+        var elementVisible = 150;
 
-// bruh.innerHTML="bruh";
+        // if element's distance from the bottom is more than elementTop, add the class active to it
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        }
+        else {
+            reveals[i].classList.remove("active");  
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+// to check the scroll position on page load
+reveal();
